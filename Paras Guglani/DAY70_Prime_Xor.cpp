81fce76c1e815ed36a089dd8a41349d27c0d78f6
paras <parasguglani1@gmail.com>
2022-07-08 02:50:36 +0530

    day 70

---
+++ Paras Guglani/DAY70_Prime_Xor.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+void pgsolve()
+{
+    int x, y;
+    cin >> x >> y;
+    int z;
+    z = x ^ y;
+    vi res;
+    if (x % 2 && y % 2)
+    {
+        res.pb(2 ^ x);
+        res.pb(2 ^ y);
+        res.pb(2);
+    }
+    else if (x % 2 && z % 2)
+    {
+        res.pb(2 ^ x);
+        res.pb(2);
+        res.pb(2 ^ z);
+    }
+    else if (y % 2 && z % 2)
+    {
+        res.pb(2);
+        res.pb(2 ^ y);
+        res.pb(2 ^ z);
+    }
+    sort(all(res));
+    rep(i, 3)
+    {
+        cout << res[i] << " ";
+    }
+    cout << "\n";
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
