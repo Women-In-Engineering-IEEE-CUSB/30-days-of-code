fb2f576f7869f0ef7d34dbc55318b1e284b1a186
paras <parasguglani1@gmail.com>
2022-07-14 22:04:49 +0530

    DAY76

---
+++ Paras Guglani/DAY76_E_Mirror_Grid.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+void pgsolve()
+{
+    int n;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            char c;
+            cin >> c;
+            a[i][j] = c;
+        }
+    }
+    int ans = 0;
+    rep(i, n)
+    {
+        rep(j, n)
+        {
+            ll d1 = a[i][j] - '0';
+            ll d2 = a[n - j - 1][i] - '0';
+            ll d3 = a[j][n - i - 1] - '0';
+            ll d4 = a[n - 1 - i][n - j - 1] - '0';
+            ll count = 0;
+            count = d1 + d2 + d3 + d4;
+            if (count >= 2)
+            {
+                ans += (4 - count);
+                a[i][j] = '1';
+                a[n - j - 1][i] = '1';
+                a[j][n - i - 1] = '1';
+                a[n - i - 1][n - j - 1] = '1';
+            }
+            else
+            {
+                ans += count;
+                a[i][j] = '0';
+                a[n - j - 1][i] = '0';
+                a[j][n - i - 1] = '0';
+                a[n - i - 1][n - j - 1] = '0';
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
